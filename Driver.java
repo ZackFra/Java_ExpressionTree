6589f31d5ff299d26ae5e7cb74dd38be76ddf3bd
ZackFra <52726500+ZackFra@users.noreply.github.com>
2019-07-09 21:56:26 -0400

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -3,8 +3,11 @@ package parsetree;
 
 public class Driver {
     public static void main(String[] args) {
+        // create a new expression tree with this expression to be parsed
         ExpressionTree tree = new ExpressionTree("32 * 3 + 2 / 5 ^    7");
+        // print the value of the expression
         System.out.println(tree.eval());
+        // print the expression in postfix notation
         System.out.println(tree);
     }
 }
